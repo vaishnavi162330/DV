e2c194a58c0d9523b1d4921ef9736d8275844ff8
Vaishnavi Nikumbh <vaishnavi.official30@gmail.com>
2024-12-30 00:46:07 +0530

    Changes Completed

--- Mahi.java
+++ Mahi.java
@@ -1,9 +1,10 @@
 class Mahi{
     public static void main(String args[]){
-
+        
+        System.out.println("I am Mahi");
         for (int i = 1; i <= 10; i++){
             System.out.println(i);
         }
-        System.out.println("I am Mahi");
+        
     }
 }
